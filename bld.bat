ccc2f27f55288155286f24ed6e37691e0045b4e8
Christian Fobel <christian@fobel.net>
2017-05-03 07:07:54 -0400

    Bump platformio version to 3.3.0

--- bld.bat
+++ bld.bat
@@ -26,6 +26,6 @@ echo @echo off> %DEACTIVATE_DIR%\platformio-env-home_dir.bat
 echo set PLATFORMIO_HOME_DIR=>> %DEACTIVATE_DIR%\platformio-env-home_dir.bat
 
 REM Install platformio from pip
-"%PYTHON%" -m pip install --no-cache "platformio==3.2.1"
+"%PYTHON%" -m pip install --no-cache "platformio==3.3.0"
 if errorlevel 1 exit 1
 :: Add more build steps here, if they are necessary.
